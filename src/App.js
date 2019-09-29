8273c554f9058244fc85f5f11d4a30ca83b783a4
Zack Chestler <zack.chestler@nwea.org>
2019-09-28 20:22:39 -0700

    add cognito info

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 import { Auth, Storage } from 'aws-amplify';
 
+Auth.configure({
+    identityPoolId: 'us-west-2:51ea7eb7-65f7-430e-930d-bfe50c04509f',
+    region: 'us-west-2'}
+);
+
 Storage.configure({
     AWSS3: {
         bucket: 'sunriver-display-s3-prod',//Your bucket name;
+        region: 'us-west-2'
     }
 });
 
