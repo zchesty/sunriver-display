8ba0f448383e04951ab96aa0022f9fd3ba1f55ee
Zack Chestler <zack.chestler@nwea.org>
2019-09-28 20:15:17 -0700

    test s3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-const AWS = require('aws-sdk');
+import { Auth, Storage } from 'aws-amplify';
 
-const accessKeyId = 'AKIAJ6EKOFVEQDNOL5LA';
-const secretAccessKey = '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';
-
-AWS.config.update({accessKeyId: accessKeyId, secretAccessKey: secretAccessKey});
-
-const s3 = new AWS.S3();
-
-let params = {
-    Bucket: "sunriver-display-s3-prod"
-};
-
-s3.listObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);
+Storage.configure({
+    AWSS3: {
+        bucket: 'sunriver-display-s3-prod',//Your bucket name;
+    }
 });
+
+Storage.list('/')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
 
 function App() {
   return (
