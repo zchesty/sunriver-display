32ee0b435c0395bcc2e2a0e138c1fd4dccbc50d4
Zack Chestler <zack.chestler@nwea.org>
2019-09-28 20:01:37 -0700

    test s3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const AWS = require('aws-sdk');
+
+const accessKeyId = 'AKIAJ6EKOFVEQDNOL5LA';
+const secretAccessKey = '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';
+
+AWS.config.update({accessKeyId: accessKeyId, secretAccessKey: secretAccessKey});
+
+const s3 = new AWS.S3();
+
+let params = {
+    Bucket: "sunriver-display-s3-prod"
+};
+
+s3.listObjects(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);
+});
 
 function App() {
   return (
