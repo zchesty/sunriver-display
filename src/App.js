9c07f7a1926cc7a8b384dba20617d71be480129a
Zack Chestler <zack.chestler@nwea.org>
2019-09-28 22:02:36 -0700

    test list

--- src/App.js
+++ src/App.js
@@ -14,27 +14,13 @@ Storage.configure({
     }
 });
 
-Storage.get('requirements.txt')
+Storage.list('public/')
     .then(result => console.log(result))
     .catch(err => console.log(err));
 
-const AWS = require('aws-sdk');
-
-const accessKeyId = 'AKIAJ6EKOFVEQDNOL5LA';
-const secretAccessKey = '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';
-
-AWS.config.update({accessKeyId: accessKeyId, secretAccessKey: secretAccessKey});
-
-const s3 = new AWS.S3();
-
-let params = {
-    Bucket: "sunriver-display-s3-prod"
-};
-
-s3.listObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);
-});
+Storage.get('requirements.txt')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
 
 function App() {
   return (
